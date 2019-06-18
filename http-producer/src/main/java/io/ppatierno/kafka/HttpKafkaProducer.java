dba6100f051cea4e8f112429ca595ed0c3cf18a0
Paolo Patierno <ppatierno@live.com>
2019-06-18 11:40:29 +0200

    Added HTTP Kafka producer logic

--- http-producer/src/main/java/io/ppatierno/kafka/HttpKafkaProducer.java
+++ http-producer/src/main/java/io/ppatierno/kafka/HttpKafkaProducer.java
@@ -1,12 +1,21 @@
 package io.ppatierno.kafka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
 
 /**
  * HttpKafkaProducer
@@ -19,6 +28,7 @@ public class HttpKafkaProducer extends AbstractVerticle {
 
     private WebClient client;
     private long sendTimer;
+    private int messagesSent;
 
     /**
      * Constructor
@@ -39,19 +49,88 @@ public class HttpKafkaProducer extends AbstractVerticle {
         this.client = WebClient.create(vertx, options);
 
         this.sendTimer = vertx.setPeriodic(this.config.getSendInterval(), t -> {
-            // TODO: send method
+            this.send(this.config.getTopic()).setHandler(ar -> {
+                if (ar.succeeded()) {
+                    log.info("Sent {}", ar.result());
+                }
+            });
         });
-
-        // TODO
         startFuture.complete();
     }
 
     @Override
     public void stop(Future<Void> stopFuture) throws Exception {
         log.info("HTTP Kafka producer stopping");
-
-        // TODO
         this.vertx.cancelTimer(this.sendTimer);
         stopFuture.complete();
+    }
+
+    private Future <List<OffsetRecordSent>> send(String topic) {
+        Future<List<OffsetRecordSent>> fut = Future.future();        
+
+        JsonObject records = new JsonObject();
+        records.put("records", new JsonArray().add(new JsonObject().put("value", "message-" + this.messagesSent++)));
+
+        this.client.post("/topics/" + topic)
+            .putHeader(HttpHeaderNames.CONTENT_LENGTH.toString(), String.valueOf(records.toBuffer().length()))
+            .putHeader(HttpHeaderNames.CONTENT_TYPE.toString(), "application/vnd.kafka.json.v2+json")
+            .as(BodyCodec.jsonObject())
+            .sendJsonObject(records, ar -> {
+                if (ar.succeeded()) {
+                    HttpResponse<JsonObject> response = ar.result();
+                    if (response.statusCode() == HttpResponseStatus.OK.code()) {
+                        List<OffsetRecordSent> list = new ArrayList<>();
+                        response.body().getJsonArray("offsets").forEach(obj -> {
+                            JsonObject json = (JsonObject) obj;
+                            list.add(new OffsetRecordSent(
+                                json.getInteger("partition"), 
+                                json.getLong("offset"))
+                                );
+                        });
+                        fut.complete(list);
+                    } else {
+                        fut.fail(new RuntimeException("Got HTTP status code " + response.statusCode()));
+                    }
+                } else {
+                    fut.fail(ar.cause());
+                }
+            });
+        return fut;
+    }
+
+    /**
+     * Represents information about a message sent
+     */
+    class OffsetRecordSent {
+
+        private final int partition;
+        private final long offset;
+
+        OffsetRecordSent(int partition, long offset) {
+            this.partition = partition;
+            this.offset = offset;
+        }
+
+        /**
+         * @return partition from which the message was received
+         */
+        public int getPartition() {
+            return partition;
+        }
+
+        /**
+         * @return message offset in the partition
+         */
+        public long getOffset() {
+            return offset;
+        }
+
+        @Override
+        public String toString() {
+            return "OffsetRecordSent(" +
+                    "partition=" + this.partition +
+                    ",offset=" + this.offset +
+                    ")";
+        }
     }
 }
